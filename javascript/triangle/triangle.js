04e9c22f05bb3faa655f4b8768a0494e539e592e
wteo <wendyteo.wy@gmail.com>
2022-03-19 21:10:00 +1100

    Refactored code

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -3,45 +3,34 @@
 // convenience to get you started writing code faster.
 //
 
+
 export class Triangle {
   constructor(...sides) {
     this.sides = sides;
-    this.isLengthGreaterThanZero();
-    this.isTwoSidesLengthGreaterThanThird(); 
+    this.isLengthPositive();
+    this.isTwoSidesGreater(); 
     this.isTriangleValid();
     this.findUniqueSides = new Set(sides);
   }
-  isLengthGreaterThanZero() {
+  isLengthPositive() {
     return this.sides.every((length) => length > 0);
   }
-  isTwoSidesLengthGreaterThanThird() {
+  isTwoSidesGreater() {
     const longestToShortest = this.sides.sort((a, b) => b - a);
     const longest = longestToShortest[0];
     const sumOfTwoLengths = longestToShortest[1] + longestToShortest[2];
     return (sumOfTwoLengths >= longest) ? true : false; 
   }
   isTriangleValid() {
-    return (
-      this.isLengthGreaterThanZero() && 
-      this.isTwoSidesLengthGreaterThanThird()
-    ) ? true : false;
+    return (this.isLengthPositive() && this.isTwoSidesGreater()) ? true : false;
   }
   get isEquilateral() {
-    return (
-      this.isTriangleValid() && 
-      this.findUniqueSides.size === 1
-    ) ? true : false;
+    return (this.isTriangleValid() && this.findUniqueSides.size === 1) ? true : false;
   }
   get isIsosceles() {
-    return (
-      this.isTriangleValid() && 
-      this.findUniqueSides.size <= 2
-    ) ? true : false;
+    return (this.isTriangleValid() && this.findUniqueSides.size <= 2) ? true : false;
   }
   get isScalene() {
-    return (
-      this.isTriangleValid() && 
-      this.findUniqueSides.size === 3
-    ) ? true : false;
+    return (this.isTriangleValid() && this.findUniqueSides.size === 3) ? true : false;
   }
 }
