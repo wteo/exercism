b0c5a2e28fdd10960dabc0eb65ce645ff08b9c17
wteo <wendyteo.wy@gmail.com>
2022-03-19 21:11:23 +1100

    Refactored code

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,7 +4,6 @@
 //
 
 
-
 export class Triangle {
   constructor(...sides) {
     this.sides = sides;
@@ -15,15 +14,15 @@ export class Triangle {
     const longestToShortest = this.sides.sort((a, b) => b - a);
     const longest = longestToShortest[0];
     const sumOfTwoLengths = longestToShortest[1] + longestToShortest[2];
-    return (isLengthPositive && sumOfTwoLengths >= longest) ? true : false; 
+    return isLengthPositive && sumOfTwoLengths >= longest; 
   }
   get isEquilateral() {
-    return (this.isTriangleValid && this.findUniqueSides.size === 1) ? true : false;
+    return this.isTriangleValid && this.findUniqueSides.size === 1;
   }
   get isIsosceles() {
-    return (this.isTriangleValid && this.findUniqueSides.size <= 2) ? true : false;
+    return this.isTriangleValid && this.findUniqueSides.size <= 2;
   }
   get isScalene() {
-    return (this.isTriangleValid && this.findUniqueSides.size === 3) ? true : false;
+    return this.isTriangleValid && this.findUniqueSides.size === 3;
   }
 }
